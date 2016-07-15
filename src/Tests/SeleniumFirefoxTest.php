a9a472c98e1319fb32e20e8c579ea12f4127f7a6
Sabine Miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2016-07-15 15:13:35 +0200

    initial commit

---
+++ src/Tests/SeleniumFirefoxTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class SeleniumFirefoxTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://google.com/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('http://www.google.com/');
+        $this->assertEquals('Google', $this->title());
+    }
+
+}
